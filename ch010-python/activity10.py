a02e55d517b3186b2a9b1d4e06e000054658f2ea
JohnK <jfkquality@gmail.com>
2017-07-16 19:08:38 -0400

    Add ch010, part of ch011

---
+++ ch010-python/activity10.py
@@ -0,0 +1,9 @@
+age = raw_input("How old are you? ")
+print "in 10 years you will be " + age
+print "In 20 years you will be ", (age + 20)
+print "Half a lifetime ago you were ", (age / 2 )
+if age >= 18:
+   print "You are eligible to vote in the US."
+else:
+   print "You are too young to vote in the US."
+    
---
+++ ch011-variables/helloWorld.py
@@ -0,0 +1,9 @@
+print "Hello World"
+print "This is my first Python program demoing Strings"
+print "Roses are {}".format("red")
+print "Violets are {}".format("blue")
+name = "JFK"
+print "My name is {}".format(name)
+print 12+5
+print 12.567
+
